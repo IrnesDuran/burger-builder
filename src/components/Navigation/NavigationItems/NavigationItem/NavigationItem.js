01ecff68bcd16689259d6079b939c39d62ffc59b
darkhitect1985 <55331764+darkhitect1985@users.noreply.github.com>
2020-01-08 22:21:57 +0100

    input component

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,7 +3,7 @@ import classes from './NavigationItem.css';
 import {NavLink} from 'react-router-dom';
 
 
-const navigationItem =(props) => (
+const NavigationItem =(props) => (
     <li className={classes.NavigationItem}>
         <NavLink
         to ={props.link} exact
@@ -13,4 +13,4 @@ const navigationItem =(props) => (
     </li>
 );
 
-export default navigationItem;
+export default NavigationItem;
